865e5a4f0b0cc3420a8f5f3195b3b14e39905fdb
CELLLO <markhamonca@codeninjas.com>
2023-05-31 16:59:36 -0400

    p
    p

---
+++ CT - Codey Raceway/Assets/Scripts/itemBoxSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxSpawner : MonoBehaviour
+{
+    public GameObject itemBox;
+    public int numberOfBoxes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int p = 0; p <= numberOfBoxes; p++)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
