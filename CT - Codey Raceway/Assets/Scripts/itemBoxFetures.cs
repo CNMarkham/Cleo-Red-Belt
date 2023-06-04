a3584fa9a99c29cebd65c28d800aa7d8acaea6c4
CELLLO <markhamonca@codeninjas.com>
2023-06-04 15:51:15 -0400

    continued need TO MARK ME ON HAZDOREUS CONTITIONS THINGY
    james halpes me

---
+++ CT - Codey Raceway/Assets/Scripts/itemBoxFetures.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBoxFetures : MonoBehaviour
+{
+
+    public float RotationSpeed = 3;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(30, 68, 49) * Time.deltaTime * RotationSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.localScale = Vector3.zero;
+            Invoke("Reappear", 2f);
+        }
+    }
+
+    private void Reappear()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+}
--- CT - Codey Raceway/Assets/Scripts/itemBoxSpawner.cs
+++ CT - Codey Raceway/Assets/Scripts/itemBoxSpawner.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class itemBoxSpawner : MonoBehaviour
 {
     public GameObject itemBox;
+
     public int numberOfBoxes;
 
+    public int modifyXposition;
+    public int modifyZposition;
 
     // Start is called before the first frame update
     void Start()
     {
         for (int p = 0; p <= numberOfBoxes; p++)
         {
-            GameObject itemBoxClone = Instantiate(itemBox, transform.position, Quaternion.identity);
-
+            GameObject itemBoxClone = Instantiate(
+                itemBox,
+                new Vector3 (
+                    transform.position.x + modifyXposition * p,
+                    transform.position.y,
+                    transform.position.z + modifyZposition * p
+                    ),
+                transform.rotation);
         }
     }
 
