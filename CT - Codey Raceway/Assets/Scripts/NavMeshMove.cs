e4d2f3f1046994403989ae9ada3337f8ec4ae6bf
CLeo <markhamonca@codeninjas.com>
2023-06-22 17:54:03 -0400

    PPOSKOSJISKM
    SAME SING

--- CT - Codey Raceway/Assets/Scripts/NavMeshMove.cs
+++ CT - Codey Raceway/Assets/Scripts/NavMeshMove.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshMove : MonoBehaviour
 {
+    public NavMeshAgent aGeNt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        aGeNt.destination = GameObject.FindGameObjectWithTag("Obstacle").transform.position;
+
+        for ( )
+    } 
+
 
     // Update is called once per frame
     void Update()
