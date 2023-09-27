7dee8b7306f1145e7545c9c3159b87a51a92fc10
Ryan <markhamonca@codeninjas.com>
2023-09-27 17:59:27 -0400

    GEERRR
    hehehe 66 or 65

--- CT - Ripoff_Angry-Birds - Copy/Assets/Scripts/LivesManager.cs
+++ CT - Ripoff_Angry-Birds - Copy/Assets/Scripts/LivesManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LivesManager : MonoBehaviour
 {
     public int lives;
     public GameObject[] hearts;
+    public GameObject slime;
 
     public void RemoveLife()
     {
-        lives -= 1;
-        hearts[lives].SetActive(false);
-        print("Imagine LOsing a Lifeeee!! Lives: " + lives);
+        if (lives > 0)
+        {
+            lives -= 1;
+            hearts[lives].SetActive(false);
+            print("Imagine LOsing a Lifeeee!! Lives: " + lives);
+        }
+
+        if (lives <= 0)
+        {
+            slime.SetActive(false);
+            SceneManager.LoadScene(0);
+        }
+
     }
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +36,6 @@ public class LivesManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
