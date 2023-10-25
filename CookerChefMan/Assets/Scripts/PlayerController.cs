89742ae162b3b656956d0f345ef8961192a9577f
Clips eohy <markhamonca@codeninjas.com>
2023-10-25 17:57:45 -0400

    boots and cats and dogs and shoes andf
    fingading big a ping
    dunmg dung
    dung dung dim dim don pim pim pim pommmmm
     step 36

---
+++ CookerChefMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+    private Quaternion lastLook;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        anim = GetComponent<Animator>();
+        lastLook = transform.rotation;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movementVector = new Vector3(horizontal, 0, vertical).normalized;
+        
+        if (movementVector.magnitude != 0)
+        {
+            lastLook = Quaternion.LookRotation(movementVector);
+        }
+        transform.rotation = lastLook;
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed;
+        rb.velocity = movement;
+        //rb.MovePosition(transform.position + movement);
+
+        anim.SetFloat("horizontalVector", movementVector.magnitude);
+        anim.SetFloat("verticalVector", 0);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("col" + collision.gameObject.name);
+    }
+
+}
